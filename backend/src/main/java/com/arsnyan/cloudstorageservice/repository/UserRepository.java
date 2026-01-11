936bd1b09d6e36a2c9f24806df1605b7fa60329c
arsnyan <arsnyan.dev@gmail.com>
2026-01-11 21:16:14 +0300

    Fix user repository queries and add unique constraint to username in users table

--- backend/src/main/java/com/arsnyan/cloudstorageservice/repository/UserRepository.java
+++ backend/src/main/java/com/arsnyan/cloudstorageservice/repository/UserRepository.java
@@ -4,15 +4,16 @@ import com.arsnyan.cloudstorageservice.model.User;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<@NonNull User, @NonNull Long> {
     @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.roles WHERE u.username = :username")
-    Optional<User> getUserByUsername(String username);
+    Optional<User> getUserByUsername(@Param("username") String username);
 
-    @Query("SELECT u.id FROM User u")
-    Optional<Long> getUserIdByUsername(String username);
+    @Query("SELECT u.id FROM User u WHERE u.username = :username")
+    Optional<Long> getUserIdByUsername(@Param("username") String username);
 
     boolean existsByUsername(String username);
 }
